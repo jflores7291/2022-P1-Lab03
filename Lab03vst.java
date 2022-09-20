6ecfb48ff88c4dbb719ed5a6461c89e66af2b8e2
15283 <jared.flores@vtsd.com>
2022-09-20 08:59:35 -0400

    100 point version jf

--- Lab03vst.java
+++ Lab03vst.java
@@ -8,8 +8,21 @@ public class Lab03vst
 {
 	public static void main(String[] args)
 	{
-      System.out.println("Lab03, 80 Point Version\n");
-      
+		System.out.println("Lab03, 100 Point Version\n");
+		int millisec = 10000123;
+		System.out.println("Starting Milli Seconds:" +millisec);
+		System.out.println("Hours :" +millisec/3600000);
+		int millisec2 = millisec%3600000;
+		System.out.println("Minutes:" +millisec2/60000);
+		int millisec3 = millisec2%60000;
+		System.out.println("Seconds:" +millisec3/1000);
+		System.out.println("Milli-seconds:" +millisec3%1000);
+
 	}
 }
+
+
+
+
+
 
